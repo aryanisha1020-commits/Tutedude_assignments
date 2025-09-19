e39d97a8950fcd8cab28d5f290476782f1d6d03d
aryanisha1020-commits <aryanisha1020@gmail.com>
2025-09-19 23:53:46 +0530

    Task 2 added

---
+++ task2nd.py
@@ -0,0 +1,8 @@
+#This take first/last name input from user 
+first_name = str(input("Enter your first name: "))
+last_name = str(input("Enter your last name: "))
+
+final_name = first_name+" "+last_name+"!"     #This is called string concatination and an empty str in between.
+
+print("Hello",final_name,"Welcome to the python program.")
+
