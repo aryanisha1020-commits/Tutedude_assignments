d8244adf10b30e22ddc839fd8b83252362579fc8
aryanisha1020-commits <aryanisha1020@gmail.com>
2025-09-19 23:26:40 +0530

    Task 1

--- FirstAssignment.py
+++ FirstAssignment.py
@@ -1,1 +1,16 @@
-#Write your answer here and uplode it.
+#This takes input value from user 
+X = float(input("Enter Your 1st value: "))
+Y = float(input("Enter Your 2nd value: "))
+
+#Performs the basic mathematical operations on these two numbers:
+
+print("Addition:",X + Y)
+print("Subtraction:",X - Y)
+print("Multipliction:",X * Y)
+
+if Y != 0:
+    print("Division:",X/Y)
+else:
+    print("Can't divide by 0")
+
+print("Thanks You")  #code end
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Tutedude_assignments
 This repository is created for assignments related work from TUTEDUDE .
+
